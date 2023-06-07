7c58fdeccb0c17177cf00df1e314448a9e731003
yellowDeerrr <emailforgithubbanksystemsql@gmail.com>
2023-06-07 17:25:27 +0300

    first commit

---
+++ src/test/java/app/tiktok/TiktokApplicationTests.java
@@ -0,0 +1,13 @@
+package app.tiktok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiktokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
